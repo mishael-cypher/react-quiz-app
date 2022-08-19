4350612d93ba7fa855a3ebc824271ed15619c5ce
Mishael <somtookonkwo96.gmail.com>
2022-08-19 16:17:42 +0100

    finished basic quiz app features

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import Timer from "./components/Timer";
+import Trivia from "./components/Trivia";
+import Start from "./components/Start";
+import data from "./data";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("$ 0");
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1000" },
+        { id: 6, amount: "$ 2000" },
+        { id: 7, amount: "$ 4000" },
+        { id: 8, amount: "$ 8000" },
+        { id: 9, amount: "$ 16000" },
+        { id: 10, amount: "$ 32000" },
+        { id: 11, amount: "$ 64000" },
+        { id: 12, amount: "$ 125000" },
+        { id: 13, amount: "$ 250000" },
+        { id: 14, amount: "$ 500000" },
+        { id: 15, amount: "$ 1000000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+  return (
+    <div className="App">
+      {username ? (
+        <>
+          <div className="main">
+            {stop ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((data) => (
+                <li
+                  className={
+                    questionNumber === data.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{data.id}</span>
+                  <span className="moneyListItemAmount">{data.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <Start setUsername={setUsername} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Start.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+
+const Start = ({setUsername}) => {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value)
+  }
+  return (
+    <div className="start">
+      <input
+        type="text"
+        className="startInput"
+        placeholder="enter your name"
+        ref={inputRef}
+      />
+      <button className="startButton" onClick={handleClick}>Start</button>
+    </div>
+  );
+};
+
+export default Start;
